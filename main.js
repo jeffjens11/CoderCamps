b5b610dba49589f77030e14153225cbae853c81d
Jeff <jeffjens11@gmail.com>
2016-10-18 23:11:28 -0700

    added checkbox javascript class styles

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-
-
-
-
-
 /*
-var chk1 = document.getElementById('box1');
-chk1.addEventListener('checked', function(){
-	//chk1.
-});
+var ul = document.getElementById("list");
+var li = ul.getElementsByTagName("li");
 
 
 
+for (var i = 0; i<li.length; i++){
+	li[i].className = "checked";
+}
 
-Move all that inline style into an external CSS file
-Oh no! Your hard drive crashed. Delete your folder and clone from GitHub.
-Add checkboxes to you list item
 Add an external JavaScript file that adds a CSS class to the label for the checkbox when the checkbox is checked, and removes the class when the checkbox is unchecked. The CSS class should apply the style text-decoration: line-through;
 
 */
+
+
+var chk1 = document.getElementById("box1");
+var chk2 = document.getElementById("box2");
+var chk3 = document.getElementById("box3");
+var chk4 = document.getElementById("box4");
+
+chk2.addEventListener('click', function(){
+	addClass(chk2);
+});
+chk3.addEventListener('click', function(){
+	addClass(chk3);
+});
+chk1.addEventListener('click', function(){
+	addClass(chk1);
+});
+chk4.addEventListener('click', function(){
+	addClass(chk4);
+});
+
+function addClass(chk){
+	if(chk.checked){
+		chk.className = "checked";
+	}else{
+		chk.className = "notChecked";
+	}
+}
+
