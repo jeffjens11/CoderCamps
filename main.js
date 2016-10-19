a3740d5447e7aa5e59fc8b07c6ce6e96e1cb4314
Jeff <jeffjens11@gmail.com>
2016-10-18 22:14:12 -0700

    further changes

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+
+
+
+
+
+/*
 var chk1 = document.getElementById('box1');
 chk1.addEventListener('checked', function(){
 	//chk1.
 });
 
+
+
+
+Make an index.html that contains the skeleton of an html page (html tag, body with Hello World)
+Change the page to contain only an unordered shopping list of items you need to eat your next meal.
+Add some inline styling to your html file (for example, center the list, change the text color, add a background image)
+Move all that inline style into an external CSS file
+Oh no! Your hard drive crashed. Delete your folder and clone from GitHub.
+Add checkboxes to you list item
+Add an external JavaScript file that adds a CSS class to the label for the checkbox when the checkbox is checked, and removes the class when the checkbox is unchecked. The CSS class should apply the style text-decoration: line-through;
+
+*/
